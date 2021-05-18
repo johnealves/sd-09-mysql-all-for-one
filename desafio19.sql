015394b40f0ec51256cd124d8620b9e173bfd59f
MarcioFujita <hanker78@gmail.com>
2021-05-18 17:50:45 -0300

    Requisito 19 corrigido e requisitos 20 ao 27

--- desafio19.sql
+++ desafio19.sql
@@ -1,1 +1,1 @@
-SELECT COUNT(shipper_id = 2) AS orders_count FROM northwind.orders WHERE employee_id IN (5, 6);
+SELECT COUNT(shipper_id) AS orders_count FROM northwind.orders WHERE shipper_id = 2 AND  employee_id IN (5, 6);
