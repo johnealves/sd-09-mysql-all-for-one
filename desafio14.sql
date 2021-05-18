9b7f5a01cff2c26b30f13a96897b6318f5736113
Jose Daniel Arreaza <jdaniell.93@gmail.com>
2021-05-18 19:35:22 -0300

    req13-19

--- desafio14.sql
+++ desafio14.sql
@@ -1,6 +1,1 @@
-SELECT 
-    supplier_id
-FROM
-    northwind.purchase_orders
-WHERE
-    supplier_id IN (1 , 3);
+SELECT supplier_id FROM northwind.purchase_orders WHERE supplier_id BETWEEN 1 and 3;
