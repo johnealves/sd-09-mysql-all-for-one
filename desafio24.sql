4a7f81f3bec13d5ed5d9947143ca8f82235e798a
BernardoFontes <bfontes.1997@gmail.com>
2021-05-18 15:33:07 -0300

    23,24,26,3

--- desafio24.sql
+++ desafio24.sql
@@ -1,1 +1,1 @@
-TRUNCATE northwind.order_details;
+UPDATE northwind.order_details SET discount = 15 WHERE unit_price > 10.0000 AND id BETWEEN 30 AND 40;
--- desafio3.sql
+++ desafio3.sql
@@ -1,5 +1,1 @@
-SELECT information_schema.KEY_COLUMN_USAGE.COLUMN_NAME as COLUMN_NAME
-FROM information_schema.KEY_COLUMN_USAGE
-WHERE information_schema.KEY_COLUMN_USAGE.CONSTRAINT_NAME LIKE "PRIMARY" AND
-information_schema.KEY_COLUMN_USAGE.TABLE_SCHEMA LIKE "northwind" AND
-information_schema.KEY_COLUMN_USAGE.TABLE_NAME LIKE "products";
+SELECT id FROM northwind.products;
