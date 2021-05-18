d31d4414f57d0b0e42f2bfb24e82ca9298f02e5c
tiago-yoneda <xyonedax@gmail.com>
2021-05-18 16:29:42 -0300

    arrumando codeClimate

--- desafio13.sql
+++ desafio13.sql
@@ -1,8 +1,7 @@
 -- 13 - Mostre o supplier_id das purchase_orders em que o supplier_id seja 1 ou 3.
-SELECT 
+SELECT
     supplier_id
 FROM
     `northwind`.`purchase_orders`
 WHERE
     supplier_id IN (1 , 3);
-    
