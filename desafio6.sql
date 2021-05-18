90daa9551e1888c8a376f9f878bb6a83a81f8390
tiago-yoneda <xyonedax@gmail.com>
2021-05-18 15:54:14 -0300

    arrumando codeClimate

--- desafio6.sql
+++ desafio6.sql
@@ -1,5 +1,4 @@
 -- 6 - Exiba os dados das colunas product_name e id da tabela products de maneira que os resultados estejam em ordem alfabética dos nomes.
 SELECT product_name, id FROM `northwind`.`products`
 ORDER BY product_name ASC;
-
 -- desnecessário usar o ASC, uma vez que é padrão, mas é bom deixar explícito para não esquecer.
