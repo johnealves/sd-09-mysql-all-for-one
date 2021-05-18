bd36f08ee74418fb4892f5ef2677864a8fc69ddd
Bob <wend3llsouza@gmail.com>
2021-05-18 19:16:06 -0300

    :nine: Desafio 9

--- testing.sh
+++ testing.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-MYSQL_USER=firstUser MYSQL_PASSWORD=12345 HOSTNAME=localhost npm test tests/initialChallenges.spec.js
+MYSQL_USER=firstUser MYSQL_PASSWORD=12345 HOSTNAME=localhost npm test tests/filteringChallenges.spec.js
