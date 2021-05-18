b90e9293f2b9d31b1b728b876fb91e38bbdf1591
Rafael Pufal <rafael.pufal23@gmail.com>
2021-05-18 15:48:31 -0300

    intermediarios com code climate resolvido

--- desafio11.sql
+++ desafio11.sql
@@ -1,2 +1,3 @@
 select notes from northwind.purchase_orders 
 where notes like '%#3_';
+
--- desafio17.sql
+++ desafio17.sql
@@ -1,2 +1,3 @@
 select id, supplier_id from northwind.purchase_orders 
-where supplier_id  in (1, 3, 5, 7);  
+where supplier_id  in (1, 3, 5, 7);
+
