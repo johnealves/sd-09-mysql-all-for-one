0677e3378208dffab0a289d4f7e7258d8ac0603e
LucioOSilva <lucioosilva00@gmail.com>
2021-05-18 16:31:45 -0300

    added beautify to requirements 9 up to 19

--- desafio17.sql
+++ desafio17.sql
@@ -1,2 +1,5 @@
-SELECT id, supplier_id FROM northwind.purchase_orders
-WHERE supplier_id % 2 <> 0 AND supplier_id < 8;
+SELECT  id
+       ,supplier_id
+FROM northwind.purchase_orders
+WHERE supplier_id % 2 <> 0 
+AND supplier_id < 8;
