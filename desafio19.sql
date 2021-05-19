db0f17f78848e531b26b2ac23c439e6aa275e11a
mayconfelipes <mayconfelipe.dev@gmail.com>
2021-05-18 22:23:23 -0300

    REQUISITO do FILTERING completos

--- desafio19.sql
+++ desafio19.sql
@@ -1,3 +1,1 @@
-SELECT count(*) AS orders_count
-FROM northwind.orders 
-WHERE employee_id = 5 or employee_id = 6 AND shipper_id = 2; 
+SELECT count(*) AS orders_count FROM northwind.orders WHERE employee_id = 5 or employee_id = 6 AND shipper_id = 2; 
