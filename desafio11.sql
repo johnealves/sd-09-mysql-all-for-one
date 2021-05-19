bedf1cf7e935c38dfd555d2c3deaced934fe66bb
MuriloBatista <mub4si@gmail.com>
2021-05-18 22:04:22 -0300

    FIX CodeClimate

--- desafio11.sql
+++ desafio11.sql
@@ -1,7 +1,2 @@
-SELECT 
-    *
-FROM
-    northwind.purchase_orders
-WHERE
-    notes BETWEEN (notes LIKE '%#30') AND (notes LIKE '%#39');
+SELECT * FROM northwind.purchase_orders WHERE notes BETWEEN (notes LIKE '%#30') AND (notes LIKE '%#39');
     
