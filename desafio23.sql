1b6210ef12bc22e75b7e245b280c6312c4b35d60
Igor Marcos Riegel <igor.riegel@gmail.com>
2021-05-19 10:14:39 -0300

    Desafios de 20 a 27 correçao testes

--- desafio23.sql
+++ desafio23.sql
@@ -1,7 +1,3 @@
-SET SQL_SAFE_UPDATES = 0;
-
 UPDATE northwind.order_details
 SET discount = 30
-WHERE unit_price < 10000;
-
-SET SQL_SAFE_UPDATES = 1;
+WHERE unit_price < 10.0000;
--- desafio26.sql
+++ desafio26.sql
@@ -1,6 +1,2 @@
-SET SQL_SAFE_UPDATES = 0;
-
 DELETE FROM northwind.order_details
-WHERE unit_price < 10000;
-
-SET SQL_SAFE_UPDATES = 1;
+WHERE unit_price > 10.0000;
