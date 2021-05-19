4c8d8cf3325f4ec39beb6ee5647377826894f5d8
Leonardo Jose da Silveira <leonardojosedasilveira@gmail.com>
2021-05-19 00:28:03 -0300

    update 18 19
    correction

--- desafio18.sql
+++ desafio18.sql
@@ -1,2 +1,2 @@
 SELECT * FROM northwind.purchase_orders
-where supplier_id like 1 and status_id like 2;
+where supplier_id like 3 and status_id like 2;
--- desafio19.sql
+++ desafio19.sql
@@ -1,3 +1,3 @@
-SELECT count(*) FROM northwind.orders
+SELECT count(*) AS 'orders_count' FROM northwind.orders
 where (employee_id like 6 or employee_id like 5)
 and shipper_id = 2;
