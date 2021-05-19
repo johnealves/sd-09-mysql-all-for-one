2513f7539574789008926e3e84d9f64869727d1a
hannibal1207 <mathausannibal@gmail.com>
2021-05-19 01:10:31 -0300

    adiciona 9 req

---
+++ desafio11.sql
@@ -0,0 +1,5 @@
+SELECT 
+    notes
+FROM
+    northwind.purchase_orders
+ WHERE id BETWEEN 96 and 100;
---
+++ desafio19.sql
@@ -0,0 +1,2 @@
+SELECT shipper_id AS orders_count FROM northwind.orders
+WHERE ((employee_id = 6 OR employee_id = 5) AND shipper_id = 2);
