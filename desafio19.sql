ad0d3f917bf684b7fad06c17e16992780322f054
hannibal1207 <mathausannibal@gmail.com>
2021-05-20 22:56:41 -0300

    adiciona req 10,19,20,21,22

--- desafio19.sql
+++ desafio19.sql
@@ -1,2 +1,2 @@
-SELECT shipper_id AS orders_count FROM northwind.orders
-WHERE ((employee_id = 6 OR employee_id = 5) AND shipper_id = 2);
+SELECT COUNT(shipper_id) AS orders_count FROM northwind.orders
+WHERE employee_id = 6 AND shipper_id = 2 OR employee_id = 5 AND shipper_id = 2;
