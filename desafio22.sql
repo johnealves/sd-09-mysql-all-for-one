5be52b64f6e1c9604555688b60414162ccdc2e36
Pedro <ped.alm2709@gmail.com>
2021-05-21 11:11:27 -0300

    correcao 22 e req 23

--- desafio22.sql
+++ desafio22.sql
@@ -1,4 +1,5 @@
 SET SQL_SAFE_UPDATES = 0;
-UPDATE northwind.order_details SET discount = 15;UPDATE northwind.order_details 
-SET 
-    discount = 15;
+UPDATE northwind.order_details SET discount = 15;
+
+SELECT discount FROM northwind.order_details;
+ 
