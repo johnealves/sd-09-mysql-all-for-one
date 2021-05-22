6b38525c5763f4e14ea86eb21d152d44f2ad1764
Caio Daniel Nunes Santos <caio.daniel.nunes.santos@gmail.com>
2021-05-22 11:44:15 -0300

    Eleven

--- desafio11.sql
+++ desafio11.sql
@@ -1,2 +1,1 @@
-SELECT notes from northwind.purchase_orders
-    WHERE notes BETWEEN 'Purchase generated based on Order #30' AND 'Purchase generated based on Order #39' ;
+SELECT submitted_date FROM northwind.purchase_orders WHERE DATE(submitted_date) = '2006-04-26';
