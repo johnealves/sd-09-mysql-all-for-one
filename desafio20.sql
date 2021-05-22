59d3ab53b34d94cc0108d258a4e17e4eb93fc9a6
Bruno <brunocmoraes97@gmail.com>
2021-05-22 14:28:28 -0300

    Requisito 20 -fix lint

--- desafio20.sql
+++ desafio20.sql
@@ -8,12 +8,12 @@ INSERT INTO northwind.order_details (order_id,
     purchase_order_id,
     inventory_id)
 VALUES (
-	69,
-    80,
-    15.0000,
-    15.0000,
-    0,
-    2,
-    NULL,
-    NULL,
-    129);
+69,
+80,
+15.0000,
+15.0000,
+0,
+2,
+NULL,
+NULL,
+129);
