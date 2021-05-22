226e1fa5f7ae7e590536a511d74c1b1e59afe64b
Samuel <samuelvanguarda@gmail.com>
2021-05-22 14:16:44 -0300

    Requisito 19 - Correção

--- desafio19.sql
+++ desafio19.sql
@@ -1,2 +1,4 @@
-SELECT COUNT(employee_id) AS 'order_counts' FROM northwind.orders WHERE employee_id IN(5, 6) AND 
-shipper_id = 2;
+SELECT COUNT(*) AS 'orders_count' FROM northwind.orders
+WHERE employee_id 
+  IN (5, 6) 
+  AND shipper_id = 2;
