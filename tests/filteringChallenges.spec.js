c3cdb8b236d3dc3a9f57f2e496558117b9145800
Bob <wend3llsouza@gmail.com>
2021-05-22 23:47:18 -0300

    :package: desafio 16 e 17

--- tests/filteringChallenges.spec.js
+++ tests/filteringChallenges.spec.js
@@ -82,7 +82,7 @@ describe('Desafios sobre filtragem de dados', () => {
     });
   });
 
-  describe.only('15 - Mostre somente as horas (sem os minutos e os segundos) da `submitted_date` de todos registros de `purchase_orders`. Chame essa coluna de `submitted_hour`.', () => {
+  describe('15 - Mostre somente as horas (sem os minutos e os segundos) da `submitted_date` de todos registros de `purchase_orders`. Chame essa coluna de `submitted_hour`.', () => {
     it('Verifica o desafio15', async () => {
       const challengeQuery = readFileSync('desafio15.sql', 'utf8').trim();
       const expectedResult = require('./challengesResults/challengeResult15');
