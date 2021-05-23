b5975ad79c94159ec23fa5ed079aab5383562df6
Enio Nicael <enionicael@gmail.com>
2021-05-23 16:32:25 -0300

    Refatoracao do desafio 13.

--- desafio13.sql
+++ desafio13.sql
@@ -1,4 +1,4 @@
-// 13 - Mostre o `supplier_id` das `purchase_orders` em que o `supplier_id` seja 1 ou 3.
+-- 13 - Mostre o `supplier_id` das `purchase_orders` em que o `supplier_id` seja 1 ou 3.
 
 SELECT 
     supplier_id
