985cf8bd8683c5956101d45724511b665ea0575a
Víctor Cançado <victor.cancado@gmail.com>
2021-05-26 17:46:32 -0300

    Corrige estilo reqs 17, 18 e 19

--- desafio17.sql
+++ desafio17.sql
@@ -1,5 +1,8 @@
 SELECT
-	id, supplier_id
+    id, supplier_id
 FROM
     northwind.purchase_orders
-WHERE supplier_id = 1 OR supplier_id = 3 OR supplier_id = 5 OR supplier_id = 7;
+WHERE
+    supplier_id = 1 OR supplier_id = 3
+        OR supplier_id = 5
+        OR supplier_id = 7;
--- desafio18.sql
+++ desafio18.sql
@@ -1,5 +1,6 @@
 SELECT 
-	*
+    *
 FROM
     northwind.purchase_orders
-WHERE supplier_id = 3 AND status_id = 2;
+WHERE
+    supplier_id = 3 AND status_id = 2;
