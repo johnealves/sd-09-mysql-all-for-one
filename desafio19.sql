014b0581dd52f436c50723406fc6b4fa83f98df1
maym4y <jennim.almeida@gmail.com>
2021-05-26 04:53:33 -0300

    Consertando desafio 19

--- desafio19.sql
+++ desafio19.sql
@@ -1,3 +1,3 @@
-SELECT * FROM northwind.purchase_orders
-WHERE supplier_id = 3
-AND status_id = 2;
+SELECT COUNT(*) AS orders_count FROM northwind.orders
+WHERE employee_id IN (5, 6)
+AND shipper_id = 2;
